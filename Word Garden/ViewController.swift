b301b3739beb27fc3e03eee6f1b355acdb1858ae
Michelle Krameisen <michellekrameisen@michelles-mbp.bc.edu>
2017-09-12 16:45:36 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Michelle Krameisen on 9/12/17.
+//  Copyright © 2017 Krameisen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
